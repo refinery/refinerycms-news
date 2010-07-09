1c05031719479487ac065bbbcef37d383d1ebcfe
Kivanio Barbosa <kivanio@gmail.com>
2010-07-09 17:36:42 -0400

    Fixed error when plugin's name are translated

--- rails/init.rb
+++ rails/init.rb
@@ -2,7 +2,8 @@ Refinery::Plugin.register do |plugin|
   plugin.title = "News"
   plugin.description = "Provides a blog-like news section"
   plugin.version = 1.0
-  plugin.menu_match = /admin\/news(_items)?$/
+  plugin.menu_match = /(admin|refinery)\/news(_items)?$/
+  plugin.url = '/refinery/news'
   plugin.activity = {
     :class => NewsItem,
     :title => 'title',
