9575321ba0a2a5906571ee730394da809c3993b1
Hez Ronningen <code@hez.ca>
2010-09-16 13:29:00 +0800

    fix index so it uses paginate correctly, call the NewsItem#published instead of NewsItem#latest

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,12 +1,17 @@
 class NewsItemsController < ApplicationController
 
   before_filter :find_latest_news_items, :find_page
+  before_filter :find_published_news_items, :find_page, :only => [:index]
   before_filter :find_news_item, :only => [:show]
 
 protected
 
   def find_latest_news_items
     @news_items = NewsItem.latest.paginate(:page => params[:page]) # 10 items
+  end
+
+  def find_published_news_items
+    @news_items = NewsItem.published.paginate(:page => params[:page])
   end
 
   def find_news_item
