8b5833f5ae8af7225d0b6ea79ed81dc99728a43f
Hez Ronningen <code@hez.ca>
2010-09-24 14:02:34 -0700

    fix public facing routes, having the full resource public is a security risk

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Refinery::Application.routes.draw do
-  resources :news, :as => :news_items, :controller => :news_items
+  resources :news, :as => :news_items, :controller => :news_items, :only => [:get, :index]
 
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
     resources :news, :as => :news_items, :controller => :news_items
