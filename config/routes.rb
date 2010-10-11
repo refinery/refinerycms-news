186f3b64b65a4e1be356a556d387b3519f57a8a6
enric lluelles <enriclluelles@gmail.com>
2010-10-11 19:40:21 +0200

    Fixed route in news_items#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Refinery::Application.routes.draw do
-  resources :news, :as => :news_items, :controller => :news_items, :only => [:get, :index]
+  resources :news, :as => :news_items, :controller => :news_items, :only => [:show, :index]
 
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
     resources :news, :as => :news_items, :controller => :news_items
   end
 end
+
