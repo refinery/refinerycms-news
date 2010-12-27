5894e020bf396fe8b3a9804a7595976b94d5f82d
David Christiansen <dave@developertown.com>
2010-12-27 17:42:23 -0600

    Fixes broken tests caused by a change in the way rails counts active record rows

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -56,12 +56,12 @@ describe NewsItem do
       5.times { Factory(:news_item, :publish_date => Time.now + 1.hour) }
       NewsItem.latest.count.should == 5
       7.times { Factory(:news_item) }
-      NewsItem.latest.count.should == 12
+      NewsItem.latest.length.should == 10
     end
 
     it "returns latest n news items" do
       4.times { Factory(:news_item) }
-      NewsItem.latest(3).count.should == 4
+      NewsItem.latest(3).length.should == 3
     end
   end
 
