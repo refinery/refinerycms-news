b8de99831081944ccb2cc8203226e810e3ecdc51
Philip Arndt <parndt@gmail.com>
2011-04-07 11:05:33 +1200

    Only need to add image_id when it doesn't already exist, some people have it already.

--- db/migrate/4_add_image_id_to_news_items.rb
+++ db/migrate/4_add_image_id_to_news_items.rb
@@ -1,7 +1,9 @@
 class AddImageIdToNewsItems < ActiveRecord::Migration
 
   def self.up
-    add_column ::NewsItem.table_name, :image_id, :integer
+    unless ::NewsItem.column_names.map(&:to_sym).include?(:image_id)
+      add_column ::NewsItem.table_name, :image_id, :integer
+    end
   end
 
   def self.down
