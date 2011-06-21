49b1af22509b45a383ccdb89123c8d9e67358203
Vladimir Krylov <s6numid@gmail.com>
2011-06-21 09:14:15 +0300

    fixes to last changes

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -34,10 +34,10 @@ protected
   end
   
   def show_preview
-    @show_preview ||= RefinerySetting.get_or_set(:refinery_news_show_preview, true)
+    @show_preview ||= RefinerySetting.find_or_set(:refinery_news_show_preview, true)
   end
   def has_image
-    @has_image ||= RefinerySetting.get_or_set(:refinery_news_has_image, true)
+    @has_image ||= RefinerySetting.find_or_set(:refinery_news_has_image, true)
   end
 
 end
