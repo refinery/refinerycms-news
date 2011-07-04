cb9d18444e1d1e1ec3cd66c88b0f3970b22878db
Uģis Ozols <ugis.ozolss@gmail.com>
2011-07-04 12:29:57 +0300

    This is not used in cucumber steps.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -5,8 +5,6 @@ module NavigationHelpers
         case page_name
         when /the list of news items/
           refinery_admin_news_items_path
-        when /the new news item form/
-          new_refinery_admin_news_item_path
         else
           begin
             if page_name =~ /the news item titled "?([^\"]*)"?/ and (news_item = Refinery::NewsItem.where(:title => $1).first).present?
