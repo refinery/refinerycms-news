c8b239866dd0e7571cd71c8b93037970a7061837
Philip Arndt <parndt@gmail.com>
2012-01-24 16:39:11 +1300

    put archive inside /news

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   scope(:module => 'refinery', :as => 'refinery') do
     scope(:path => '', :module => 'news', :as => 'news') do
-      get 'archive/:year(/:month)', :to => 'items#archive', :as => 'items_archive'
-
       scope(:path => 'news') do
+        get 'archive/:year(/:month)', :to => 'items#archive', :as => 'items_archive'
         resources :items, :only => [:show, :index], :path => ''
       end
 
