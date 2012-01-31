fd50337f7a44eeb3778ae08cb4e4b3c375c2976f
Philip Arndt <parndt@gmail.com>
2012-01-31 15:11:56 +1300

    Converted scopes to class methods.

--- app/models/refinery/news/item.rb
+++ app/models/refinery/news/item.rb
@@ -15,42 +15,6 @@ module Refinery
 
       default_scope :order => "publish_date DESC"
 
-      scope :by_archive, lambda { |archive_date|
-        where(['publish_date between ? and ?', archive_date.beginning_of_month, archive_date.end_of_month])
-      }
-
-      scope :by_year, lambda { |archive_year|
-        where(['publish_date between ? and ?', archive_year.beginning_of_year, archive_year.end_of_year])
-      }
-
-      scope :all_previous, lambda { where(['publish_date <= ?', Time.now.beginning_of_month]) }
-
-      scope :previous, lambda { |i| where("publish_date < ?", i.publish_date).limit(1) }
-
-      # If you're using a named scope that includes a changing variable you need to wrap it in a lambda
-      # This avoids the query being cached thus becoming unaffected by changes (i.e. Time.now is constant)
-      scope :not_expired, lambda {
-        news_items = Arel::Table.new(self.table_name)
-        where(news_items[:expiration_date].eq(nil).or(news_items[:expiration_date].gt(Time.now)))
-      }
-      scope :published, lambda {
-        not_expired.where("publish_date < ?", Time.now)
-      }
-      scope :latest, lambda { |*l_params|
-        published.limit( l_params.first || 10)
-      }
-
-      scope :live, lambda { not_expired.where( "publish_date <= ?", Time.now) }
-
-      # rejects any page that has not been translated to the current locale.
-      scope :translated, lambda {
-        pages = Arel::Table.new(self.table_name)
-        translations = Arel::Table.new(self.translations_table_name)
-
-        includes(:translations).where(
-          translations[:locale].eq(Globalize.locale)).where(pages[:id].eq(translations[:refinery_news_item_id]))
-      }
-
       def not_published? # has the published date not yet arrived?
         publish_date > Time.now
       end
@@ -64,10 +28,55 @@ module Refinery
       end
 
       class << self
-        def next(current_record)
+        def by_archive(archive_date)
+          where(['publish_date between ? and ?', archive_date.beginning_of_month, archive_date.end_of_month])
+        end
+
+        def by_year(archive_year)
+          where(['publish_date between ? and ?', archive_year.beginning_of_year, archive_year.end_of_year])
+        end
+
+        def all_previous
+          where(['publish_date <= ?', Time.now.beginning_of_month])
+        end
+
+        def next(item)
           self.send(:with_exclusive_scope) do
-            where("publish_date > ?", current_record.publish_date).order("publish_date ASC")
+            where("publish_date > ?", item.publish_date).order("publish_date ASC")
           end
+        end
+
+        def previous(item)
+          where("publish_date < ?", item.publish_date).first
+        end
+
+        def not_expired
+          news_items = Arel::Table.new(self.table_name)
+          where(news_items[:expiration_date].eq(nil).or(news_items[:expiration_date].gt(Time.now)))
+        end
+
+        def published
+          not_expired.where("publish_date < ?", Time.now)
+        end
+
+        def latest(limit = 10)
+          published.limit(limit)
+        end
+
+        def live
+          not_expired.where( "publish_date <= ?", Time.now)
+        end
+
+        # rejects any page that has not been translated to the current locale.
+        def translated
+          pages = Arel::Table.new(self.table_name)
+          translations = Arel::Table.new(self.translations_table_name)
+
+          includes(:translations).where(
+            translations[:locale].eq(Globalize.locale)
+          ).where(
+            pages[:id].eq(translations[:refinery_news_item_id])
+          )
         end
 
         def teasers_enabled?
