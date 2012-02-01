13a1756caaa7e507c2aee9916c542e9572d04d45
Philip Arndt <parndt@gmail.com>
2012-02-01 19:25:33 +1300

    Fixed remaining specs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Refinery::Core::Engine.routes.draw do
-  namespace :news, :path => '' do
+  namespace :news do
     root :to => "items#index"
     get 'archive/:year(/:month)', :to => 'items#archive', :as => 'items_archive'
     resources :items, :only => [:show, :index], :path => ''
+  end
 
+  namespace :news, :path => '' do
     namespace :admin, :path => 'refinery' do
       scope :path => 'news' do
         root :to => "items#index"
--- spec/requests/visit_news_items_spec.rb
+++ spec/requests/visit_news_items_spec.rb
@@ -6,8 +6,8 @@ describe "visit news items" do
   before(:each) do
     Factory.create(:page, :link_url => "/")
     Factory.create(:page, :link_url => "/news", :title => "News")
-    Factory.create(:news_item, :title => "published", :source => "http://refinerycms.com")
-    Factory.create(:news_item, :title => "unpublished", :publish_date => 1.day.from_now)
+    @published_news_item = Factory.create(:news_item, :title => "published", :source => "http://refinerycms.com", :publish_date => 1.hour.ago)
+    @unpublished_news_item = Factory.create(:news_item, :title => "unpublished", :publish_date => 1.day.from_now)
   end
 
   it "shows news link in menu" do
@@ -24,8 +24,14 @@ describe "visit news items" do
 
     page.should have_content("published")
     page.should have_selector("a[href='/news/published']")
-    page.should have_content("Source http://refinerycms.com")
+
     page.should have_no_content("unpublished")
     page.should have_no_selector("a[href='/news/unpublished']")
+  end
+
+  it "has a source on the news item" do
+    visit refinery.news_item_path(@published_news_item)
+
+    page.should have_content("Source http://refinerycms.com")
   end
 end
