e5812fca67caf28f0a2d5d880bfdbc93a062af52
Johannes Edelstam <johannes@edelst.am>
2012-02-01 13:33:25 +0100

    public routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  namespace :news do
-    root :to => "items#index"
-    get 'archive/:year(/:month)', :to => 'items#archive', :as => 'items_archive'
-    resources :items, :only => [:show, :index], :path => ''
-  end
-
   scope(:as => 'refinery', :module => 'refinery') do
     scope(:module => 'news', :as => 'news') do
+      scope path: '/news' do
+        root :to => "items#index"
+        get 'archive/:year(/:month)', :to => 'items#archive', :as => 'items_archive'
+        resources :items, :only => [:show, :index], :path => ''
+      end
 
       scope(:module => 'admin', :path => 'refinery/news', :as => 'admin') do
         resources :items, :path => '', :except => :show
