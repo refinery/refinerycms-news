f549c4ffef216017ad96b30cdd4e59be8413d0e1
Rob Yurkowski <rob@godynamo.com>
2012-02-02 16:08:52 -0500

    Fixes namespacing in migrations.

--- db/migrate/20120129230839_translate_source.rb
+++ db/migrate/20120129230839_translate_source.rb
@@ -2,14 +2,14 @@
 class TranslateSource < ActiveRecord::Migration
 
   def up
-    unless Refinery::NewsItem::Translation.column_names.map(&:to_sym).include?(:source)
-      add_column Refinery::NewsItem::Translation.table_name, :source, :string
+    unless Refinery::News::Item::Translation.column_names.map(&:to_sym).include?(:source)
+      add_column Refinery::News::Item::Translation.table_name, :source, :string
     end
   end
 
   def down
-    if Refinery::NewsItem::Translation.column_names.map(&:to_sym).include?(:source)
-      remove_column Refinery::NewsItem::Translation.table_name, :source
+    if Refinery::News::Item::Translation.column_names.map(&:to_sym).include?(:source)
+      remove_column Refinery::News::Item::Translation.table_name, :source
     end
   end
 
