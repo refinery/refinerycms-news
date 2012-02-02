f48383da7477b608ecba156797a9eb39791f1e96
Uģis Ozols <ugis.ozolss@gmail.com>
2012-02-02 14:22:16 +0200

    Append seed loading to main app seeds.rb.

--- lib/generators/refinery/news_generator.rb
+++ lib/generators/refinery/news_generator.rb
@@ -3,5 +3,16 @@ module Refinery
     def rake_db
       rake("refinery_news:install:migrations")
     end
+
+    def append_load_seed_data
+      create_file 'db/seeds.rb' unless File.exists?(File.join(destination_root, 'db', 'seeds.rb'))
+      append_file 'db/seeds.rb', :verbose => true do
+        <<-EOH
+
+# Added by RefineryCMS News engine
+Refinery::News::Engine.load_seed
+        EOH
+      end
+    end
   end
 end
