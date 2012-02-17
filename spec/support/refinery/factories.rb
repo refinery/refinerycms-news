5c055d404cdf90d47046eb6b92d076b6cdf65368
Johannes Edelstam <johannes@edelst.am>
2012-02-17 13:08:51 +0100

    Only define the factory if it doesn't exist already

--- spec/support/refinery/factories.rb
+++ spec/support/refinery/factories.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
-  factory :news_item, :class => Refinery::News::Item do
-    title "RefineryCMS News Item"
-    content "Some random text ..."
-    publish_date Time.now - 5.minutes
+  unless FactoryGirl.factories.registered?(:news_item)
+    factory :news_item, :class => Refinery::News::Item do
+      title "RefineryCMS News Item"
+      content "Some random text ..."
+      publish_date Time.now - 5.minutes
+    end
   end
 end
