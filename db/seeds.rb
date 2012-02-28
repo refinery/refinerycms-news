535954300e5530b366bcc762d7a68143975b510d
Uģis Ozols <ugis.ozolss@gmail.com>
2012-02-28 20:57:42 +0200

    Don't use 'config' when accessing configūration.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ if defined?(::Refinery::Page)
       :menu_match => "^/news.*$"
     )
 
-    ::Refinery::Pages.config.default_parts.each do |default_page_part|
+    ::Refinery::Pages.default_parts.each do |default_page_part|
       page.parts.create(:title => default_page_part, :body => nil)
     end
   end
