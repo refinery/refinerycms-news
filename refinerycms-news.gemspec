3379f1e266e7065dbd208871ee9ebb83dd37469a
Brice Sanchez <brice@bisscomm.com>
2015-02-23 22:00:44 -0500

    Update for rails 4.2.0
    Remove useless .rbenv dot files
    
    Use binstubs for travis config
    
    Chmod 755 to rake and rspec

--- refinerycms-news.gemspec
+++ refinerycms-news.gemspec
@@ -15,7 +15,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency    'refinerycms-core',     '~> 3.0.0'
   s.add_dependency    'refinerycms-settings', '~> 3.0.0'
-  s.add_dependency    'friendly_id',          '~> 5.0.1'
-  s.add_dependency    'globalize',            '~> 4.0.0'
+  s.add_dependency    'friendly_id',          '~> 5.1.0'
+  s.add_dependency    'globalize',            ['>= 4.0.0', '< 5.2']
   s.add_dependency    'acts_as_indexed',      '~> 0.8.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,20 +7,17 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 
 require 'rspec/rails'
 require 'capybara/rspec'
-require 'factory_girl_rails'
 
 Rails.backtrace_cleaner.remove_silencers!
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 end
 
 # set javascript driver for capybara
-require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
+Capybara.javascript_driver = :selenium
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories including factories.
